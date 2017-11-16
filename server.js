a6d8e1853059a6747f27d9fa245f874b05347433
JOAO OCTAVIO FULGENCIO DE OLIVEIRA <joao.ofo@puccampinas.edu.br>
2017-11-16 09:54:53 -0200

    Added Git Ignore and MatLAB connection

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
+  mongoDB = 'mongodb://admin:admin@ds064198.mlab.com:64198/prezado0';
   Dificuldade = require('./api/models/dificuldadesModel'),
   Ranking = require('./api/models/rankingModel'),
   bodyParser = require('body-parser');
   
   mongoose.Promise = global.Promise;
-  mongoose.connect('mongodb://localhost/Topicos');
-  
+  mongoose.connect(mongoDB, {
+    useMongoClient: true
+  });
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'MongoDB connection error: '));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   
