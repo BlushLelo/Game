2d3a63a033a3ba8b43f23f81d1d2d0bd3d7657d3
ext.joao.oliveira <joaooctf@ciandt.com>
2017-11-22 18:11:08 -0200

    Login header set

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -6,38 +6,13 @@ User = mongoose.model('Registro');
 
 exports.getUser = function(req, res) {
     var Ruser = new User(req.body);
-    Ruser.findOne({ login: user.login }, function (err, user) {
+    Ruser.findOne({ login: req.header('login') }, function (err, user) {
         if (err)
             res.send(err);
         else
-        if (Ruser.password === user.password){
+        if (Ruser.password === req.header('password')){
             console.log("Deu bom");
         } else
             res.send(err);
-    });
-};
-
-
-
-exports.registrar = function(req, res) {
-	var novoRegistro = new Registro(req.body);
-    Registro.findOne({ login: novoRegistro.login }, function (err, registro) {
-        if (err)
-            res.send(err);
-        else
-            if(registro) {
-            if (novoRegistro.login !== registro.login){
-                novoRegistro.save(function(err, register) {
-                    if (err)
-                        res.send(err);
-                    res.json(register);
-                });
-            }
-            }else
-                novoRegistro.save(function(err, register) {
-                    if (err)
-                        res.send(err);
-                    res.json(register);
-                });
     });
 };
